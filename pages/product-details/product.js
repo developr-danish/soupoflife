89f11a525902129bba6114107ac35e47d5f05561
danish4994 <danish0ahmad@gmail.com>
2024-03-29 12:18:30 +0530

    "updated"

--- pages/product-details/product.js
+++ pages/product-details/product.js
@@ -1,19 +1,28 @@
+// added to cart notification
 const decreaseNumber = () => {
   var itemval = document.getElementById('textbox');
+  var price = document.getElementById('productPrice')
+  var totalprice = document.getElementById('totalPrice')
   if (itemval.value <= 1) {
     itemval.value = 1;
   } else {
     itemval.value = parseInt(itemval.value) - 1;
+    price.innerHTML = parseInt(price.innerHTML) - 599.00;
+    totalprice.innerHTML = price.innerHTML;
   }
 };
 
 
 const increasNumber = () => {
-    var itemval = document.getElementById('textbox');
+    var itemval = document.getElementById('textbox');var totalprice = document.getElementById('totalPrice')
+    var price = document.getElementById('productPrice')
+    var totalprice = document.getElementById('totalPrice')
     if (itemval.value >= 5) {
         alert("Max 5 Quantity Allowed")
     } else {
       itemval.value = parseInt(itemval.value) + 1;
+      price.innerHTML = parseInt(price.innerHTML) + 599.00;
+      totalprice.innerHTML = price.innerHTML;
     }
   };
   
